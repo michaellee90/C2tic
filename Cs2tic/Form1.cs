320c49e7016ae3cf4ff4ac94973ad4d6047851c6
michaellee90 <123027844+michaellee90@users.noreply.github.com>
2023-02-07 08:29:00 -0800

    Add files via upload

---
+++ Cs2tic/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cs2tic
+{
+    public partial class Form1 : Form
+    {
+        Player currentPlayer;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public enum Player
+        {
+            X,
+            O
+        }
+
+        private void buttonClick(object sender, EventArgs e)
+        {
+            var button = (Button)sender;
+            currentPlayer = Player.X;
+            button.Text = currentPlayer.ToString();
+            button.Enabled = false;
+            button.BackColor = System.Drawing.Color.LightGreen;
+            Check();
+            AITIMER.Start();
+        }
+
+        private void playAI(object sender, EventArgs e)
+        {
+            foreach (Control x in this.Controls)
+            {
+                if (x is Button && x.Text == "?" && x.Enabled)
+                {
+                    x.Enabled = false;
+                    currentPlayer = Player.O;
+                    x.Text = currentPlayer.ToString();
+                    x.BackColor = System.Drawing.Color.LightGoldenrodYellow;
+                    AITIMER.Stop();
+                    Check();
+                    break;
+                }
+                else
+                {
+                    AITIMER.Stop();
+                }
+            }
+        }
+
+        private void resetGame(object sender, EventArgs e)
+        {
+            label1.Text = "??";
+
+            foreach (Control x in this.Controls)
+            {
+
+                if (x is Button && x.Tag == "play")
+                {
+                    ((Button)x).Enabled = true;
+                    ((Button)x).Text = "?";
+                    ((Button)x).BackColor = default(Color);
+                }
+
+            }
+        }
+
+        private void Check()
+        {
+            if (
+            button1.Text == "X" && button2.Text == "X" && button3.Text == "X" ||
+            button4.Text == "X" && button5.Text == "X" && button6.Text == "X" ||
+            button7.Text == "X" && button8.Text == "X" && button9.Text == "X" ||
+            button1.Text == "X" && button4.Text == "X" && button7.Text == "X" ||
+            button2.Text == "X" && button5.Text == "X" && button8.Text == "X" ||
+            button3.Text == "X" && button6.Text == "X" && button9.Text == "X" ||
+            button1.Text == "X" && button5.Text == "X" && button9.Text == "X" ||
+            button3.Text == "X" && button5.Text == "X" && button7.Text == "X"
+                )
+            {
+                WON();
+                label1.Text = "X Wins";
+            }
+            else if (
+            button1.Text == "O" && button2.Text == "O" && button3.Text == "O" ||
+            button4.Text == "O" && button5.Text == "O" && button6.Text == "O" ||
+            button7.Text == "O" && button8.Text == "O" && button9.Text == "O" ||
+            button1.Text == "O" && button4.Text == "O" && button7.Text == "O" ||
+            button2.Text == "O" && button5.Text == "O" && button8.Text == "O" ||
+            button3.Text == "O" && button6.Text == "O" && button9.Text == "O" ||
+            button1.Text == "O" && button5.Text == "O" && button9.Text == "O" ||
+            button3.Text == "O" && button5.Text == "O" && button7.Text == "O"
+                )
+            {
+                WON();
+                label1.Text = "O Wins";
+            }
+        }
+
+        private void WON()
+        {
+            foreach (Control x in this.Controls)
+            {
+
+                if (x is Button && x.Tag == "play")
+                {
+                    ((Button)x).Enabled = false;
+                    ((Button)x).BackColor = default(Color);
+
+                }
+
+            }
+        }
+    }
+}
